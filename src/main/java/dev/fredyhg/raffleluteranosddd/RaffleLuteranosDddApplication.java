1ee0fcfd5d943abc811fc1bc05fb9292778f2da6
Fredy <fredyultima2@gmail.com>
2024-06-04 22:44:27 -0300

    feat: att app

---
+++ src/main/java/dev/fredyhg/raffleluteranosddd/RaffleLuteranosDddApplication.java
@@ -0,0 +1,13 @@
+package dev.fredyhg.raffleluteranosddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaffleLuteranosDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaffleLuteranosDddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/fredyhg/raffleluteranosddd/RaffleLuteranosDddApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fredyhg.raffleluteranosddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaffleLuteranosDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

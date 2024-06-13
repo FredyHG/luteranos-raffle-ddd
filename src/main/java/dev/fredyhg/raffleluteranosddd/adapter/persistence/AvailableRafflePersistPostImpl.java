2119549c56d3808244387019f79c55688f20f35c
fredyhg <fredyultima2@gmail.com>
2024-06-12 22:19:38 -0300

    create AvailableRafflePersistPostImpl

---
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/AvailableRafflePersistPostImpl.java
@@ -0,0 +1,11 @@
+package dev.fredyhg.raffleluteranosddd.adapter.persistence;
+
+import dev.fredyhg.raffleluteranosddd.domain.models.availableraffle.AvailableRaffle;
+import dev.fredyhg.raffleluteranosddd.domain.ports.AvailableRafflePersistPort;
+
+public class AvailableRafflePersistPostImpl implements AvailableRafflePersistPort {
+    @Override
+    public void save(AvailableRaffle availableRaffle) {
+
+    }
+}

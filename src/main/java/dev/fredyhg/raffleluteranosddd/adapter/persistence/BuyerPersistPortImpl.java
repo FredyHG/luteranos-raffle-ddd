93341398dd2b49e898eceee332c0271feff172aa
fredyhg <fredyultima2@gmail.com>
2024-06-12 22:19:50 -0300

    create BuyerPersistPortImpl

---
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/BuyerPersistPortImpl.java
@@ -0,0 +1,11 @@
+package dev.fredyhg.raffleluteranosddd.adapter.persistence;
+
+import dev.fredyhg.raffleluteranosddd.domain.models.buyer.Buyer;
+import dev.fredyhg.raffleluteranosddd.domain.ports.BuyerPersistPort;
+
+public class BuyerPersistPortImpl implements BuyerPersistPort {
+    @Override
+    public void save(Buyer buyer) {
+
+    }
+}

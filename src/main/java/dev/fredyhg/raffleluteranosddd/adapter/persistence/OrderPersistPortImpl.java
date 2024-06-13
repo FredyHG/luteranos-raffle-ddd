f5ed70aad7500bf76f54a35db3002901c417188a
fredyhg <fredyultima2@gmail.com>
2024-06-12 22:20:01 -0300

    create OrderPersistPortImpl

---
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/OrderPersistPortImpl.java
@@ -0,0 +1,11 @@
+package dev.fredyhg.raffleluteranosddd.adapter.persistence;
+
+import dev.fredyhg.raffleluteranosddd.domain.models.Order;
+import dev.fredyhg.raffleluteranosddd.domain.ports.OrderPersistPort;
+
+public class OrderPersistPortImpl implements OrderPersistPort {
+    @Override
+    public void save(Order order) {
+
+    }
+}

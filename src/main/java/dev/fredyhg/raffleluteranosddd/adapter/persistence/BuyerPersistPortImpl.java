614403359f0033e02db279d3c3583b9766d6390a
fredyhg <fredyultima2@gmail.com>
2024-06-12 22:22:50 -0300

    added annotation @Component @RequiredArgsConstructor

--- src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/BuyerPersistPortImpl.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/BuyerPersistPortImpl.java
@@ -2,7 +2,11 @@ package dev.fredyhg.raffleluteranosddd.adapter.persistence;
 
 import dev.fredyhg.raffleluteranosddd.domain.models.buyer.Buyer;
 import dev.fredyhg.raffleluteranosddd.domain.ports.BuyerPersistPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class BuyerPersistPortImpl implements BuyerPersistPort {
     @Override
     public void save(Buyer buyer) {

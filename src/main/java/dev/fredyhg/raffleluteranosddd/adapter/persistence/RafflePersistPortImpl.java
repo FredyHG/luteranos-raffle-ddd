3a80a49a6d5c5677b122af09ceb898786fb6a4fa
fredyhg <fredyultima2@gmail.com>
2024-06-12 22:20:15 -0300

    create RafflePersistPortImpl

---
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/RafflePersistPortImpl.java
@@ -0,0 +1,11 @@
+package dev.fredyhg.raffleluteranosddd.adapter.persistence;
+
+import dev.fredyhg.raffleluteranosddd.domain.models.raffle.Raffle;
+import dev.fredyhg.raffleluteranosddd.domain.ports.RafflePersistPort;
+
+public class RafflePersistPortImpl implements RafflePersistPort {
+    @Override
+    public void save(Raffle raffle) {
+
+    }
+}

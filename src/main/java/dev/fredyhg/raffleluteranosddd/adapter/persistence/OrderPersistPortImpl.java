c5381c89b7897bcbd4b38d033c773346aa427055
fredyhg <fredyultima2@gmail.com>
2024-06-12 22:33:15 -0300

    inject repository

--- src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/OrderPersistPortImpl.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/OrderPersistPortImpl.java
@@ -1,5 +1,6 @@
 package dev.fredyhg.raffleluteranosddd.adapter.persistence;
 
+import dev.fredyhg.raffleluteranosddd.adapter.persistence.repository.OrderRepository;
 import dev.fredyhg.raffleluteranosddd.domain.models.Order;
 import dev.fredyhg.raffleluteranosddd.domain.ports.OrderPersistPort;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,9 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class OrderPersistPortImpl implements OrderPersistPort {
+
+    private final OrderRepository orderRepository;
+
     @Override
     public void save(Order order) {
 

63b81707d8329dce3d6f26ad060941383db8c85d
fredyhg <fredyultima2@gmail.com>
2024-06-12 22:22:44 -0300

    added annotation @Component @RequiredArgsConstructor

--- src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/RafflePersistPortImpl.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/RafflePersistPortImpl.java
@@ -2,7 +2,11 @@ package dev.fredyhg.raffleluteranosddd.adapter.persistence;
 
 import dev.fredyhg.raffleluteranosddd.domain.models.raffle.Raffle;
 import dev.fredyhg.raffleluteranosddd.domain.ports.RafflePersistPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class RafflePersistPortImpl implements RafflePersistPort {
     @Override
     public void save(Raffle raffle) {

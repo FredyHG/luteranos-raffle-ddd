3fb1d681232e5ba9f85cbe662cfb4850ea80d89e
fredyhg <fredyultima2@gmail.com>
2024-06-12 22:22:52 -0300

    added annotation @Component @RequiredArgsConstructor

--- src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/AvailableRafflePersistPostImpl.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/AvailableRafflePersistPostImpl.java
@@ -2,7 +2,11 @@ package dev.fredyhg.raffleluteranosddd.adapter.persistence;
 
 import dev.fredyhg.raffleluteranosddd.domain.models.availableraffle.AvailableRaffle;
 import dev.fredyhg.raffleluteranosddd.domain.ports.AvailableRafflePersistPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class AvailableRafflePersistPostImpl implements AvailableRafflePersistPort {
     @Override
     public void save(AvailableRaffle availableRaffle) {

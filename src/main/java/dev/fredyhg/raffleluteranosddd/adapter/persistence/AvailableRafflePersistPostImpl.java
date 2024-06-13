8fc67506b3aa9f940f6030572c702502db6f921c
fredyhg <fredyultima2@gmail.com>
2024-06-12 22:33:20 -0300

    inject repository

--- src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/AvailableRafflePersistPostImpl.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/AvailableRafflePersistPostImpl.java
@@ -1,5 +1,6 @@
 package dev.fredyhg.raffleluteranosddd.adapter.persistence;
 
+import dev.fredyhg.raffleluteranosddd.adapter.persistence.repository.AvailableRaffleRepository;
 import dev.fredyhg.raffleluteranosddd.domain.models.availableraffle.AvailableRaffle;
 import dev.fredyhg.raffleluteranosddd.domain.ports.AvailableRafflePersistPort;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,9 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class AvailableRafflePersistPostImpl implements AvailableRafflePersistPort {
+
+    private final AvailableRaffleRepository availableRaffleRepository;
+
     @Override
     public void save(AvailableRaffle availableRaffle) {
 

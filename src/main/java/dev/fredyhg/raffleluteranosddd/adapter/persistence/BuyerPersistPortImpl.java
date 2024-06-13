b78fc094eb95d01c8a57526e4969f647635cb924
fredyhg <fredyultima2@gmail.com>
2024-06-12 22:33:18 -0300

    inject repository

--- src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/BuyerPersistPortImpl.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/BuyerPersistPortImpl.java
@@ -1,5 +1,6 @@
 package dev.fredyhg.raffleluteranosddd.adapter.persistence;
 
+import dev.fredyhg.raffleluteranosddd.adapter.persistence.repository.BuyerRepository;
 import dev.fredyhg.raffleluteranosddd.domain.models.buyer.Buyer;
 import dev.fredyhg.raffleluteranosddd.domain.ports.BuyerPersistPort;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,9 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class BuyerPersistPortImpl implements BuyerPersistPort {
+
+    private final BuyerRepository buyerRepository;
+
     @Override
     public void save(Buyer buyer) {
 

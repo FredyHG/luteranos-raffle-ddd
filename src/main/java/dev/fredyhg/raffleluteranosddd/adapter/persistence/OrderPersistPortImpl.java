0dbb4ea5b56530dfb4b5d9cb50c76f7edae536d1
fredyhg <fredyultima2@gmail.com>
2024-06-12 22:22:48 -0300

    added annotation @Component @RequiredArgsConstructor

--- src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/OrderPersistPortImpl.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/OrderPersistPortImpl.java
@@ -2,7 +2,11 @@ package dev.fredyhg.raffleluteranosddd.adapter.persistence;
 
 import dev.fredyhg.raffleluteranosddd.domain.models.Order;
 import dev.fredyhg.raffleluteranosddd.domain.ports.OrderPersistPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class OrderPersistPortImpl implements OrderPersistPort {
     @Override
     public void save(Order order) {

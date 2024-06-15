efb8e383a163e2befd205866ca17f04499cc9938
Fredy <fredyultima2@gmail.com>
2024-06-15 10:37:54 -0300

    feat: create imgur service to upload image

---
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/service/ImgurImageUploadService.java
@@ -0,0 +1,11 @@
+package dev.fredyhg.raffleluteranosddd.adapter.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImgurImageUploadService implements ImageUploadService {
+    @Override
+    public String uploadImage(String base64Image) {
+        return "";
+    }
+}

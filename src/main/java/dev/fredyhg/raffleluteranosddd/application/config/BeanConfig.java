67de78f7ea8461a86350de3c31e37d3f47d8eff8
Fredy <fredyultima2@gmail.com>
2024-06-16 10:13:45 -0300

    feat: add more bean

--- src/main/java/dev/fredyhg/raffleluteranosddd/application/config/BeanConfig.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/application/config/BeanConfig.java
@@ -1,6 +1,8 @@
 package dev.fredyhg.raffleluteranosddd.application.config;
 
+import dev.fredyhg.raffleluteranosddd.domain.models.availableraffle.ReceiveRequestAvailableRafflePortImpl;
 import dev.fredyhg.raffleluteranosddd.domain.models.raffle.ReceiveRequestRafflePortImpl;
+import dev.fredyhg.raffleluteranosddd.domain.ports.AvailableRafflePersistPort;
 import dev.fredyhg.raffleluteranosddd.domain.ports.RafflePersistPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +11,13 @@ import org.springframework.context.annotation.Configuration;
 public class BeanConfig {
 
     @Bean
-    public ReceiveRequestRafflePortImpl saveRaffleService(RafflePersistPort rafflePersistPort) {
+    public ReceiveRequestRafflePortImpl receiveRequestRafflePortImpl(RafflePersistPort rafflePersistPort) {
         return new ReceiveRequestRafflePortImpl(rafflePersistPort);
+    }
+
+    @Bean
+    public ReceiveRequestAvailableRafflePortImpl receiveRequestAvailableRafflePort(AvailableRafflePersistPort availableRafflePersistPort) {
+        return new ReceiveRequestAvailableRafflePortImpl(availableRafflePersistPort);
     }
 
 }

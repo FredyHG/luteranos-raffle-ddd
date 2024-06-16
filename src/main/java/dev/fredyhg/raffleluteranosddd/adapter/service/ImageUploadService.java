c22bf0315169e3bc2709c8993dba0c38e7b9f765
Fredy <fredyultima2@gmail.com>
2024-06-16 10:12:59 -0300

    feat: remove import

--- src/main/java/dev/fredyhg/raffleluteranosddd/adapter/service/ImageUploadService.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/service/ImageUploadService.java
@@ -1,7 +1,5 @@
 package dev.fredyhg.raffleluteranosddd.adapter.service;
 
-import org.springframework.web.multipart.MultipartFile;
-
 public interface ImageUploadService {
     String uploadImage(String base64Image);
 }

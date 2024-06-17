8d59145d3b04f3a4e04147957e324467fb75254c
Fredy <fredyultima2@gmail.com>
2024-06-17 16:30:29 -0300

    feat: create save buyer use case

---
+++ src/main/java/dev/fredyhg/raffleluteranosddd/usecase/SaveBuyerUseCase.java
@@ -0,0 +1,22 @@
+package dev.fredyhg.raffleluteranosddd.usecase;
+
+import dev.fredyhg.raffleluteranosddd.common.dto.BuyerDto;
+import dev.fredyhg.raffleluteranosddd.common.mapper.BuyerMapper;
+import dev.fredyhg.raffleluteranosddd.domain.models.buyer.Buyer;
+import dev.fredyhg.raffleluteranosddd.domain.models.buyer.BuyerDtoReceiverPortImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SaveBuyerUseCase {
+
+    private final BuyerDtoReceiverPortImpl buyerDtoReceiverPortImpl;
+
+    public Buyer save(BuyerDto buyerDto) {
+        Buyer buyer = BuyerMapper.toBuyer(buyerDto);
+
+        return buyerDtoReceiverPortImpl.save(buyer);
+    }
+
+}

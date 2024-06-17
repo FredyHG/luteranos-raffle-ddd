5a5ea4ca97c3eb952338bf47c60121d0c70a6569
Fredy <fredyultima2@gmail.com>
2024-06-17 16:28:51 -0300

    feat: create buyer dto

---
+++ src/main/java/dev/fredyhg/raffleluteranosddd/common/dto/BuyerDto.java
@@ -0,0 +1,10 @@
+package dev.fredyhg.raffleluteranosddd.common.dto;
+
+import lombok.Data;
+
+@Data
+public class BuyerDto {
+    private String name;
+    private String email;
+    private String cpf;
+}

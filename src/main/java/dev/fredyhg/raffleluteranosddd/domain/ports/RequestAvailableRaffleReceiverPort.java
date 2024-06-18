0bb92c3ca701f8cdb2d9bffc57359249c54d1aa3
Fredy <fredyultima2@gmail.com>
2024-06-18 10:15:04 -0300

    feat: change name

--- src/main/java/dev/fredyhg/raffleluteranosddd/domain/ports/RequestAvailableRaffleReceiverPort.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/domain/ports/RequestAvailableRaffleReceiverPort.java
@@ -2,6 +2,6 @@ package dev.fredyhg.raffleluteranosddd.domain.ports;
 
 import dev.fredyhg.raffleluteranosddd.domain.models.availableraffle.AvailableRaffle;
 
-public interface ReceiveRequestAvailableRafflePort {
+public interface RequestAvailableRaffleReceiverPort {
     AvailableRaffle save(AvailableRaffle availableRaffle);
 }

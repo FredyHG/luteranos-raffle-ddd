11d72ea24922b9ac85090116070014460af7110c
Fredy <fredyultima2@gmail.com>
2024-06-18 10:21:22 -0300

    feat: add more methods in controller

--- src/main/java/dev/fredyhg/raffleluteranosddd/infrastructure/http/controller/RaffleController.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/infrastructure/http/controller/RaffleController.java
@@ -1,16 +1,19 @@
 package dev.fredyhg.raffleluteranosddd.infrastructure.http.controller;
 
+import dev.fredyhg.raffleluteranosddd.adapter.persistence.model.RaffleModel;
 import dev.fredyhg.raffleluteranosddd.infrastructure.http.request.AvailableRafflePostRequest;
+import dev.fredyhg.raffleluteranosddd.infrastructure.http.response.AvailableRaffleGetResponse;
 import dev.fredyhg.raffleluteranosddd.infrastructure.http.response.ResponseMessage;
+import dev.fredyhg.raffleluteranosddd.usecase.FindAvailableRaffleUseCase;
+import dev.fredyhg.raffleluteranosddd.usecase.FindRafflesByRaffleTypeUseCase;
 import dev.fredyhg.raffleluteranosddd.usecase.SaveAvailableRaffleUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -21,6 +24,8 @@ import java.time.LocalDateTime;
 public class RaffleController {
 
     private final SaveAvailableRaffleUseCase saveAvailableRaffleUseCase;
+    private final FindAvailableRaffleUseCase findAvailableRaffleUseCase;
+    private final FindRafflesByRaffleTypeUseCase findRafflesByRaffleTypeUseCase;
 
     @PostMapping
     public ResponseEntity<ResponseMessage> createRaffle(@RequestBody AvailableRafflePostRequest availableRafflePostRequest) {
@@ -35,5 +40,16 @@ public class RaffleController {
                 .timestamp(LocalDateTime.now())
                 .build());
     }
+
+    @GetMapping("/{raffleType}")
+    public ResponseEntity<Page<RaffleModel>> findAvailableRequest(@PathVariable String raffleType, Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(findRafflesByRaffleTypeUseCase.findRafflesByRaffleType(raffleType , pageable));
+    }
+
+    @GetMapping("/available-raffles")
+    public ResponseEntity<Page<AvailableRaffleGetResponse>> findAvailableRequest(Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(findAvailableRaffleUseCase.findAvailableRaffles(pageable));
+    }
+
 
 }

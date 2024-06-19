521b81a0a834bd71892c0125b2c60b40c0da02b9
Fredy <fredyultima2@gmail.com>
2024-06-19 09:58:43 -0300

    feat: create integration tests

--- src/test/java/dev/fredyhg/raffleluteranosddd/RaffleLuteranosDddApplicationTests.java
+++ src/test/java/dev/fredyhg/raffleluteranosddd/RaffleLuteranosDddApplicationTests.java
@@ -1,13 +1,8 @@
 package dev.fredyhg.raffleluteranosddd;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RaffleLuteranosDddApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
---
+++ src/test/java/dev/fredyhg/raffleluteranosddd/infrastructure/http/controller/OrderControllerIntegrationTest.java
@@ -0,0 +1,27 @@
+package dev.fredyhg.raffleluteranosddd.infrastructure.http.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class OrderControllerIntegrationTest {
+
+//    public void testGet() {
+//        given()
+//                .header("Content-type", "application/json")
+//                .and()
+//                .body(requestBody)
+//                .get("/availableTags")
+//                .then()
+//                .statusCode(200)
+//                .body("numberOfElements", equalTo(1));
+//
+//    }
+
+}

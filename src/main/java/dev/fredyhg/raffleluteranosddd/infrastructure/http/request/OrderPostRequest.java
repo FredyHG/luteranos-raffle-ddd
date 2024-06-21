8ca1599e3d43de50e76676b97ee16695b6fa6993
Fredy <fredyultima2@gmail.com>
2024-06-21 10:12:42 -0300

    feat: add constructor

--- src/main/java/dev/fredyhg/raffleluteranosddd/infrastructure/http/request/OrderPostRequest.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/infrastructure/http/request/OrderPostRequest.java
@@ -1,14 +1,14 @@
 package dev.fredyhg.raffleluteranosddd.infrastructure.http.request;
 
 import dev.fredyhg.raffleluteranosddd.common.dto.BuyerDto;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class OrderPostRequest {
-
     private List<String> rafflesIds;
-
     private BuyerDto buyer;
 }

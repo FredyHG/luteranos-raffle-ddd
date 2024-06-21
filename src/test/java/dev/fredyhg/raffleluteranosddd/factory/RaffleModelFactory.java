3a1014066c955a898353212cb26bd97c2b47c692
Fredy <fredyultima2@gmail.com>
2024-06-21 09:49:16 -0300

    feat: create empty constructor in factory

--- src/test/java/dev/fredyhg/raffleluteranosddd/factory/RaffleModelFactory.java
+++ src/test/java/dev/fredyhg/raffleluteranosddd/factory/RaffleModelFactory.java
@@ -4,11 +4,15 @@ import dev.fredyhg.raffleluteranosddd.adapter.persistence.model.RaffleModel;
 import org.instancio.Instancio;
 import org.instancio.Model;
 
+import java.util.UUID;
+
 import static org.instancio.Select.field;
 
 public class RaffleModelFactory {
     private static final Model<RaffleModel> VALID_RAFFLE_MODEL =
             Instancio.of(RaffleModel.class)
+                    .set(field(RaffleModel::getId), UUID.randomUUID().toString())
+                    .set(field(RaffleModel::getOrder), null)
                     .toModel();
 
     public static RaffleModel withValidName(String name) {

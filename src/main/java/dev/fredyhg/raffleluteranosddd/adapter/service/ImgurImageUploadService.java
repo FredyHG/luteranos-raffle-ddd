520da260697f33de89705de5f0f11bddd4eb9cf5
Fredy <fredyultima2@gmail.com>
2024-06-21 10:02:35 -0300

    feat: create logic of method

--- src/main/java/dev/fredyhg/raffleluteranosddd/adapter/service/ImgurImageUploadService.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/service/ImgurImageUploadService.java
@@ -1,11 +1,23 @@
 package dev.fredyhg.raffleluteranosddd.adapter.service;
 
+import dev.fredyhg.raffleluteranosddd.adapter.service.feign.ImageClient;
+import dev.fredyhg.raffleluteranosddd.application.properties.ImgurProperties;
+import dev.fredyhg.raffleluteranosddd.common.dto.ImageResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ImgurImageUploadService implements ImageUploadService {
+
+    private static final String TYPE = "base64";
+
+    private final ImageClient imageClient;
+
+    private final ImgurProperties imgurProperties;
+
     @Override
-    public String uploadImage(String base64Image) {
-        return "";
+    public ImageResponse uploadImage(String imageBase64, String title) {
+        return imageClient.uploadImage("Client-ID " + imgurProperties.getClientId(), TYPE, imageBase64, title);
     }
 }

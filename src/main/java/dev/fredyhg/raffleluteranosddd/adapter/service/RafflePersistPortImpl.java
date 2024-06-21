17bbf33b03db888683845604e4a3ab2d42b9abec
Fredy <fredyultima2@gmail.com>
2024-06-21 10:12:56 -0300

    feat: improve save all method

--- src/main/java/dev/fredyhg/raffleluteranosddd/adapter/service/RafflePersistPortImpl.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/service/RafflePersistPortImpl.java
@@ -8,30 +8,18 @@ import dev.fredyhg.raffleluteranosddd.domain.ports.RafflePersistPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.List;
 
-    @Component
-    @RequiredArgsConstructor
+@Component
+@RequiredArgsConstructor
 public class RafflePersistPortImpl implements RafflePersistPort {
 
     private final RaffleRepository raffleRepository;
-
-    private final ImageUploadService imageUploadService;
-
     @Override
     public void saveAll(List<Raffle> raffles) {
 
-        HashMap<String, String> nameUrlImage = new HashMap<>();
-
-        for (Raffle raffle : raffles) {
-            String imageUrl = imageUploadService.uploadImage(raffle.getImageBase64());
-            nameUrlImage.put(raffle.getName(), imageUrl);
-        }
-
-        List<RaffleModel> listRafflesToBeSaved = raffles.stream().map(
-                raffle -> RaffleMapper.toRaffleModel(raffle, nameUrlImage.get(raffle.getName()))
-        ).toList();
+        List<RaffleModel> listRafflesToBeSaved =
+                raffles.stream().map(RaffleMapper::toRaffleModel).toList();
 
         raffleRepository.saveAll(listRafflesToBeSaved);
     }

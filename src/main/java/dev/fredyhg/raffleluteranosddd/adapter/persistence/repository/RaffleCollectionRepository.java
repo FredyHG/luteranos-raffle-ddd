683ba202d27259811a55c82471ece79583bad756
Fredy <fredyultima2@gmail.com>
2024-06-21 16:09:08 -0300

    fix: fix and rename test

--- src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/repository/RaffleCollectionRepository.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/repository/RaffleCollectionRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RaffleCollectionRepository extends JpaRepository<RaffleCollectionModel, String> {
-    Optional<RaffleCollectionModel> findByRaffleType(String raffleType);
+    Optional<RaffleCollectionModel> findByCollectionName(String collectionName);
 }
--- src/test/java/dev/fredyhg/raffleluteranosddd/infrastructure/http/controller/OrderControllerIntegrationTest.java
+++ src/test/java/dev/fredyhg/raffleluteranosddd/infrastructure/http/controller/OrderControllerIntegrationTest.java
@@ -42,7 +42,7 @@ class OrderControllerIntegrationTest {
     private final Gson gson = new Gson();
 
     @Test
-    void testGet() {
+    void testCreateOrderSuccessfully() {
 
         BuyerModel savedBuyer = buyerRepository.save(BuyerModelFactory.valid());
 

c66b783f3ecb11cbcb70a6c340bd85f2ab7f9190
Fredy <fredyultima2@gmail.com>
2024-06-23 15:28:53 -0300

    fix: rename method

--- src/main/java/dev/fredyhg/raffleluteranosddd/application/usecase/FindRafflesByRaffleTypeUseCase.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/application/usecase/FindRafflesByRaffleTypeUseCase.java
@@ -17,10 +17,10 @@ public class FindRafflesByRaffleTypeUseCase {
     private final RaffleRepository raffleRepository;
     private final RaffleCollectionRepository raffleCollectionRepository;
 
-    public Page<RaffleModel> findRafflesByRaffleType(String typeRaffle, Pageable pageable){
+    public Page<RaffleModel> findRafflesByCollectionName(String collectionName, Pageable pageable){
 
         RaffleCollectionModel raffleCollection = raffleCollectionRepository
-                .findByCollectionName(typeRaffle).orElseThrow(
+                .findByCollectionName(collectionName).orElseThrow(
                         () -> new RaffleNotFoundException("Raffle not found"));
 
         return raffleRepository.findByRaffleId(raffleCollection.getId(), pageable);

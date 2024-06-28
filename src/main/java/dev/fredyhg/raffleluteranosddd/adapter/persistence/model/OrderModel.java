8d87d69989ea6b3576d43ba2386cfa7e0d932482
fredyhg <fredyultima2@gmail.com>
2024-06-28 18:00:46 -0300

    feat: create more attributes in order model

--- src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/model/OrderModel.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/adapter/persistence/model/OrderModel.java
@@ -4,6 +4,7 @@ import dev.fredyhg.raffleluteranosddd.domain.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,15 +18,28 @@ public class OrderModel {
 
     @Id
     private String id;
+
+    private String collectionId;
+
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<RaffleModel> raffles;
     private LocalDateTime buyAt;
     private String buyerId;
     private BigDecimal total;
+
+    @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    public OrderModel(List<RaffleModel> raffles, String buyerId, String id) {
+    private LocalDateTime creatAt;
+
+    @PrePersist
+    private void onCreate(){
+        this.creatAt = LocalDateTime.now();
+    }
+
+    public OrderModel(List<RaffleModel> raffles, String buyerId, String id, String collectionId) {
         this.id = id;
+        this.collectionId = collectionId;
         this.status = OrderStatus.WAIT_PAYMENT;
         this.buyAt = null;
         this.raffles = raffles;

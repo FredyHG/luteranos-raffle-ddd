b5c359b842a34d25200eb177b272d8e20bf019d9
fredyhg <fredyultima2@gmail.com>
2024-06-28 18:03:04 -0300

    feat: change name of attribute in raffle collection post request

--- src/main/java/dev/fredyhg/raffleluteranosddd/infrastructure/http/request/RaffleCollectionPostRequest.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/infrastructure/http/request/RaffleCollectionPostRequest.java
@@ -1,16 +1,18 @@
 package dev.fredyhg.raffleluteranosddd.infrastructure.http.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class RaffleCollectionPostRequest {
-    private String raffleType;
+    @JsonProperty("collection_name")
+    private String collectionName;
     private List<RafflePostRequest> raffles;
 
     public RaffleCollectionPostRequest(String raffleType, List<RafflePostRequest> raffles) {
-        this.raffleType = raffleType;
+        this.collectionName = raffleType;
         this.raffles = raffles;
     }
 }

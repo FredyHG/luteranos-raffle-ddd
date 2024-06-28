2f920bfed87bdbc727e3db262e7c7815b68a34a8
fredyhg <fredyultima2@gmail.com>
2024-06-28 18:01:08 -0300

    feat: implement validation in order post request

--- src/main/java/dev/fredyhg/raffleluteranosddd/infrastructure/http/request/OrderPostRequest.java
+++ src/main/java/dev/fredyhg/raffleluteranosddd/infrastructure/http/request/OrderPostRequest.java
@@ -1,6 +1,9 @@
 package dev.fredyhg.raffleluteranosddd.infrastructure.http.request;
 
 import dev.fredyhg.raffleluteranosddd.common.dto.BuyerDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,6 +12,10 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 public class OrderPostRequest {
+    @NotBlank(message = "Collection id wasn't informed")
+    private String collectionId;
+    @NotEmpty(message = "Raffles id cannot be empty")
     private List<String> rafflesIds;
+    @NotNull(message = "Buyer id cannot be null")
     private BuyerDto buyer;
 }

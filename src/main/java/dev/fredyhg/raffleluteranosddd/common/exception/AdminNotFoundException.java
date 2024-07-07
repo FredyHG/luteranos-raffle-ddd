423dbc67a10d097bdd87f9b3e56cabf37f55b262
fredyhg <fredyultima2@gmail.com>
2024-07-06 23:15:45 -0300

    feat: create admin notfound exception

---
+++ src/main/java/dev/fredyhg/raffleluteranosddd/common/exception/AdminNotFoundException.java
@@ -0,0 +1,7 @@
+package dev.fredyhg.raffleluteranosddd.common.exception;
+
+public class AdminNotFoundException extends AdminException{
+    public AdminNotFoundException(String msg) {
+        super(msg);
+    }
+}

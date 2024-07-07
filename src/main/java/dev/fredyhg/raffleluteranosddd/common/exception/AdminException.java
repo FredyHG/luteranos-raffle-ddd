28119d660c104527a644e2e07ce68bbd3748b138
fredyhg <fredyultima2@gmail.com>
2024-07-06 23:15:35 -0300

    feat: create admin exception

---
+++ src/main/java/dev/fredyhg/raffleluteranosddd/common/exception/AdminException.java
@@ -0,0 +1,7 @@
+package dev.fredyhg.raffleluteranosddd.common.exception;
+
+public class AdminException extends RuntimeException {
+    public AdminException(String msg) {
+        super(msg);
+    }
+}

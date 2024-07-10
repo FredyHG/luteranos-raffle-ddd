6620d0f229d999b05785a001116306ac8b52f482
fredyhg <fredyultima2@gmail.com>
2024-07-09 22:30:10 -0300

    feat: create admin mapper

---
+++ src/main/java/dev/fredyhg/raffleluteranosddd/common/mapper/AdminMapper.java
@@ -0,0 +1,18 @@
+package dev.fredyhg.raffleluteranosddd.common.mapper;
+
+import dev.fredyhg.raffleluteranosddd.domain.models.admin.Admin;
+import dev.fredyhg.raffleluteranosddd.infrastructure.security.persist.model.AdminModel;
+
+public class AdminMapper {
+    public static AdminModel toModel(Admin admin) {
+        return new AdminModel(admin.getPassword(),
+                admin.getUsername(),
+                admin.getRole());
+    }
+
+    public static Admin modelToAdmin(AdminModel adminModel) {
+        return new Admin(adminModel.getId(),
+                adminModel.getUsername(),
+                adminModel.getPassword());
+    }
+}

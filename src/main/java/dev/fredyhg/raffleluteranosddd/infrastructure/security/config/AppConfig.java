6ce407dbbe4d2461ffe5c54561c556985fe01cca
fredyhg <fredyultima2@gmail.com>
2024-07-12 22:48:55 -0300

    feat: implement more configs of security

---
+++ src/main/java/dev/fredyhg/raffleluteranosddd/infrastructure/security/config/AppConfig.java
@@ -0,0 +1,50 @@
+package dev.fredyhg.raffleluteranosddd.infrastructure.security.config;
+
+import dev.fredyhg.raffleluteranosddd.common.exception.AdminNotFoundException;
+import dev.fredyhg.raffleluteranosddd.infrastructure.security.repository.AdminRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig {
+
+    private final AdminRepository adminRepository;
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> adminRepository.findByUsername(username)
+                .orElseThrow(() -> new AdminNotFoundException("Admin not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}

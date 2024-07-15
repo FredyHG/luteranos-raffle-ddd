d1d0ebe8e79a2da41c894cbe85f7fad512868fa6
fredyhg <fredyultima2@gmail.com>
2024-07-14 23:02:07 -0300

    feat: admin post request

---
+++ src/main/java/dev/fredyhg/raffleluteranosddd/infrastructure/http/request/AdminPostRequest.java
@@ -0,0 +1,9 @@
+package dev.fredyhg.raffleluteranosddd.infrastructure.http.request;
+
+import lombok.Getter;
+
+@Getter
+public class AdminPostRequest {
+    private String username;
+    private String password;
+}

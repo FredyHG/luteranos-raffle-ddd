53278b33a109da3c89fd520cbeb8f501a5e2da18
fredyhg <fredyultima2@gmail.com>
2024-07-15 21:33:22 -0300

    feat: create exception for invalid token

---
+++ src/main/java/dev/fredyhg/raffleluteranosddd/common/exception/InvalidTokenException.java
@@ -0,0 +1,7 @@
+package dev.fredyhg.raffleluteranosddd.common.exception;
+
+public class InvalidTokenException extends SecurityException {
+    public InvalidTokenException(String msg) {
+        super(msg);
+    }
+}

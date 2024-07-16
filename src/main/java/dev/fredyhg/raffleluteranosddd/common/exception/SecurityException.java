db868856bb7a0695353ac400cd86cafb0ac78320
fredyhg <fredyultima2@gmail.com>
2024-07-15 21:33:11 -0300

    feat: create generic exception for security

---
+++ src/main/java/dev/fredyhg/raffleluteranosddd/common/exception/SecurityException.java
@@ -0,0 +1,7 @@
+package dev.fredyhg.raffleluteranosddd.common.exception;
+
+public class SecurityException extends RuntimeException {
+    public SecurityException(String msg) {
+        super(msg);
+    }
+}
